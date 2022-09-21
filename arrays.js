b49b55464214cee2dfa3f26bc71d79b96fa7c6f9
Ryan Baer <ryanjbaer@outlook.com>
2022-09-21 09:32:38 -0600

    Include index demonstration

--- arrays.js
+++ arrays.js
@@ -13,3 +13,12 @@ console.log(speed)
 //.shift removes the removes the first element of an array and returns the new length of the array
 isOld.shift()
 console.log(isOld)
+
+// Index positions begin at zero and move updward with each element in the array
+//For example I'll create an array below and call the [0] index
+var exampleArray = ["Apple","Pizza","Tacos"]
+console.log(exampleArray[0])
+//If done correctly the output should be "Apple"
+//After 0 each the index 1 and goes up one at a time for however many elements are in the array
+//Since there are 3 elements in this array if I call the index of 2, it should return "Tacos"
+console.log(exampleArray[2])
