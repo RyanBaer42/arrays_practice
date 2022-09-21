ab942d687aa0ab1e353f76b983feccb2d833f010
Ryan Baer <ryanjbaer@outlook.com>
2022-09-21 09:24:31 -0600

    Add array methods

--- arrays.js
+++ arrays.js
@@ -3,3 +3,13 @@ var family = ["Jenna","Jim","Karen","Krista"]
 var speed = [78,81,65,60]
 
 var isOld = [true,false,false,true]
+
+//.pop removes the last element in an array
+family.pop()
+console.log(family)
+//.push adds one or more elements to the end of an array
+speed.push(88)
+console.log(speed)
+//.shift removes the removes the first element of an array and returns the new length of the array
+isOld.shift()
+console.log(isOld)
